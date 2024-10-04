9240321210ec991c834d2effa53a4f34e55e5016
Sylvia051202 <158839902+Sylvia051202@users.noreply.github.com>
2024-10-04 14:26:08 +0800

    结构基础

--- app/src/main/java/com/example/changli_planet_app/Activity/MainActivity.kt
+++ app/src/main/java/com/example/changli_planet_app/Activity/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.changli_planet_app
+package com.example.changli_planet_app.Activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.changli_planet_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
