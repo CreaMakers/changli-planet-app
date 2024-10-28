23e25c5f1c044c92e886176d76b81ab410035108
Sylvia051202 <158839902+Sylvia051202@users.noreply.github.com>
2024-10-28 10:04:59 +0800

    [v1.06] 登录界面和主界面UI编写

--- app/src/main/java/com/example/changli_planet_app/Activity/MainActivity.kt
+++ app/src/main/java/com/example/changli_planet_app/Activity/MainActivity.kt
@@ -5,20 +5,29 @@ import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.TableLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.changli_planet_app.Fragment.Feature
 import com.example.changli_planet_app.R
 import com.example.changli_planet_app.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
+import java.sql.Time
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    private val tabLayout : TabLayout by lazy { binding.tabLayout }
+    private var startTime:Long = 0
+    private var endTime:Long= 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        startTime = System.currentTimeMillis()
         binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
@@ -27,19 +36,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        initFragment(Feature.newInstance())
         //为导航栏设置动画和颜色
-        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
             override fun onTabSelected(p0: TabLayout.Tab?) {
                 if (p0 != null) {
                     animateTabSelect(p0)
                 }
             }
             override fun onTabUnselected(p0: TabLayout.Tab?) {
-
             }
+
             override fun onTabReselected(p0: TabLayout.Tab?) {
             }
         })
+        endTime = System.currentTimeMillis()
+        val duration = endTime - startTime
+        Log.d("MyUI","CreatTime:${duration}")
+    }
+    private fun initFragment(fragment: Fragment){
+        val fragmentationTemp = supportFragmentManager
+        val transactions = fragmentationTemp.beginTransaction()
+        transactions.replace(R.id.frag,fragment).commit()
     }
     fun animateTabSelect(tab : TabLayout.Tab){
         val tabView = tab.view
