503fb24bfe519cec8768bf10da5de44f4f1ac203
dcelysia <6873058@qq.com>
2024-12-25 13:48:46 +0800

    feat:侧边栏UI设计

--- app/src/main/java/com/example/changli_planet_app/Activity/MainActivity.kt
+++ app/src/main/java/com/example/changli_planet_app/Activity/MainActivity.kt
@@ -19,6 +19,7 @@ import com.google.android.material.tabs.TabLayout.Tab
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private val tabLayout : TabLayout by lazy { binding.tabLayout }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
