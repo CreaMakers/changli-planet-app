166dfcc5716da1ed4d2da0043b5144b99ae0824f
ChenYan <2762127550@qq.com>
2024-12-30 04:35:40 +0800

    feat: 在课表页面，增加首次登录绑定学号功能

--- app/src/main/java/com/example/changli_planet_app/Activity/TimeTableActivity.kt
+++ app/src/main/java/com/example/changli_planet_app/Activity/TimeTableActivity.kt
@@ -3,6 +3,7 @@ package com.example.changli_planet_app.Activity
 import java.lang.reflect.Type
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.provider.Settings
@@ -18,9 +19,11 @@ import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.ScrollView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.changli_planet_app.Activity.Action.TimeTableAction
@@ -30,6 +33,7 @@ import com.example.changli_planet_app.Cache.Room.CoursesDataBase
 import com.example.changli_planet_app.Data.jsonbean.GetCourse
 import com.example.changli_planet_app.Cache.Room.MySubject
 import com.example.changli_planet_app.Cache.UserInfoManager
+import com.example.changli_planet_app.Core.Route
 import com.example.changli_planet_app.R
 import com.example.changli_planet_app.UI.TimetableWheelBottomDialog
 import com.example.changli_planet_app.databinding.ActivityTimeTableBinding
@@ -96,6 +100,12 @@ class TimeTableActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        if (studentId.isEmpty() || studentPassword.isEmpty()) {
+            showMessage("请先绑定学号和密码")
+            Route.goBindingUser(this)
+            finish()
+            return
         }
         dataBase = CoursesDataBase.getDatabase(PlanetApplication.appContext)
         // 初始化 TimetableView
@@ -437,6 +447,27 @@ class TimeTableActivity : AppCompatActivity() {
         val matchResult = regex.find(weekString)
         return matchResult?.value?.toInt() ?: 0
     }
+    private fun showMessage(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).apply {
+            val cardView = CardView(applicationContext).apply {
+                radius = 25f
+                cardElevation = 8f
+                setCardBackgroundColor(getColor(R.color.score_bar))
+                useCompatPadding = true
+            }
 
+            val textView = TextView(applicationContext).apply {
+                text = message
+                textSize = 17f
+                setTextColor(Color.BLACK)
+                gravity = Gravity.CENTER
+                setPadding(80, 40, 80, 40)
+            }
+            cardView.addView(textView)
+            setGravity(Gravity.BOTTOM or Gravity.CENTER_HORIZONTAL, 0, 140)
+            view = cardView
+            show()
+        }
+    }
 
 }
