a87d4c118cd34e8aef8046a0590c1ef9125540ab
hemou6 <184505870+hemou6@users.noreply.github.com>
2025-04-30 17:46:17 +0800

    feat:更新一下版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.changli_planet_app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.1"
+        versionCode = 6
+        versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
