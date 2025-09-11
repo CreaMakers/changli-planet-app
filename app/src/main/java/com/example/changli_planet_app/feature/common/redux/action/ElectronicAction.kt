dd3cae6839023f0523a62c56b05a3885704411cb
Qingyue <2923717811@qq.com>
2025-09-12 00:56:06 +0800

    feat：添加电量查询小组件

--- app/src/main/java/com/example/changli_planet_app/feature/common/redux/action/ElectronicAction.kt
+++ app/src/main/java/com/example/changli_planet_app/feature/common/redux/action/ElectronicAction.kt
@@ -9,4 +9,5 @@ sealed class ElectronicAction {
     data class selectAddress(val address:String):ElectronicAction()
     data class selectBuild(val buildId:String):ElectronicAction()
     data class queryElectronic(val checkElectricity: CheckElectricity):ElectronicAction()
+
 }
