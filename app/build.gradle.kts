240690a6086578987e438eac66e313428a26847b
timeisthe <202408130230@stu.csust.edu.cn>
2025-09-30 21:04:41 +0800

    feature:将电量查询请求替换为从网络库获得

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -189,7 +189,7 @@ dependencies {
     implementation("androidx.work:work-rxjava2:2.10.3")
     implementation(project(":CP_Common"))
     //csustDataGet
-    implementation("com.github.CreaMakers:CSUSTDataGet:5c190ab7d1")
+    implementation("com.github.CreaMakers:CSUSTDataGet:345ff5d8c4")
 }
 kapt {
     arguments {
