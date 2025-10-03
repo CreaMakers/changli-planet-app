04d7e446038f23aed5e8c3bb93372a827a768279
wxy242 <2660200533@qq.com>
2025-10-03 12:39:03 +0800

    feature:引用最新的网络库版本
    feature:引用最新的网络库版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -189,7 +189,7 @@ dependencies {
     implementation("androidx.work:work-rxjava2:2.10.3")
     implementation(project(":CP_Common"))
     //csustDataGet
-    implementation("com.github.wxy242:CSUSTDataGet:925b73d03e")
+    implementation("com.github.CreaMakers:CSUSTDataGet:925b73d03e")
 }
 kapt {
     arguments {
