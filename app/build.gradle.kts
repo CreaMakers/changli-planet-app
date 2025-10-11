cc1b6e3f7f45936226188dd386e02eb976992c48
dcelysia <6873058@qq.com>
2025-10-12 00:02:51 +0800

    feat: 1. 更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.example.changli_planet_app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.2.3"
+        versionCode = 12
+        versionName = "1.2.4"
 
 
         ndk {
