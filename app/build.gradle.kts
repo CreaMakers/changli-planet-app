fb34a1cf3f6d0a82368e2fdc95896064e9def5b6
timeisthe <202408130230@stu.csust.edu.cn>
2025-11-02 11:44:07 +0800

    feature:增加二级评论相关文件及资源文件

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -190,7 +190,7 @@ dependencies {
     implementation(project(":CP_Common"))
     //csustDataGet
 //    implementation("com.github.CreaMakers:CSUSTDataGet:28c105c2a7")
-    implementation("com.github.CreaMakers:CSUSTDataGet:749935c885")
+    implementation("com.github.CreaMakers:CSUSTDataGet:58b4369498")
 }
 kapt {
     arguments {
