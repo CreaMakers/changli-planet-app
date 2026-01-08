17703d91cbadb090bbab5dbca62d3e49d8e4944c
dcelysia <6873058@qq.com>
2026-01-08 22:05:26 +0800

    feat: 1. 包体积优化

--- csust_spider/src/main/java/com/dcelysia/csust_spider/education/data/remote/model/GradeComponent.kt
+++
@@ -1,7 +0,0 @@
-package com.dcelysia.csust_spider.education.data.remote.model
-
-data class GradeComponent(
-    val type: String, //成绩类型
-    val grade: Double, //成绩
-    val ratio: Int //成绩比例
-)
--- csust_spider/src/main/java/com/dcelysia/csust_spider/education/data/remote/model/StudyMode.kt
+++
@@ -1,6 +0,0 @@
-package com.dcelysia.csust_spider.education.data.remote.model
-
-enum class StudyMode(val id: String) {
-    MAJOR("0"),
-    MINOR("1")
-}
