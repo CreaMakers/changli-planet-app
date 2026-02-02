b91d5e49ff04bfc0e59738d57dec5c6f6834481d
Trainer <14728057+tloveumamusume@user.noreply.gitee.com>
2026-02-02 13:27:10 +0800

    Revert "Reapply "仅修改部分""
    This reverts commit 054afcc1a188914b9aaef75b2f6eb0fc77ee3f27.

--- app/src/main/java/com/creamaker/changli_planet_app/core/PlanetApplication.kt
+++ app/src/main/java/com/creamaker/changli_planet_app/core/PlanetApplication.kt
@@ -51,7 +51,7 @@ class PlanetApplication : Application() {
         const val UserIp: String = "https://user.csust.creamaker.cn"
         const val ToolIp: String = "https://web.csust.creamaker.cn"
 // //       const val FreshNewsIp: String = "http://freshnews.csust.creamaker.cn"
-//   //     const val ToolIp: String = "http://10.0.2.2:8081/app/tools"
+//        const val ToolIp: String = "http://10.0.2.2:8081/app/tools"
 
         val preRequestIps = listOf(
             "https://user.csust.creamaker.cn",
